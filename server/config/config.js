a1d3ee34f77fae033e38cc90fc14d7af04778fc5
Jim <jim_lubbers@me.com>
2018-10-07 09:27:55 +0200

    Added test cases for POST /users & /users/me

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ if (env === 'development') {
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 } else if(env === 'test') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+  process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
 } else {
   process.env.MONGODB_URI = 'mongodb://lub-dev:database01@ds219983.mlab.com:19983/node-todo-api';
 }
